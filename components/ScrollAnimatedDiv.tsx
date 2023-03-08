9b712e463ceec4759072d9c8b087fe14be78b98c
santischuckmann <schuckmannsantiago@gmail.com>
2023-03-08 16:40:50 -0300

    feat: bringing new idea for onScroll animations

---
+++ components/ScrollAnimatedDiv.tsx
@@ -0,0 +1,45 @@
+import { useEffect } from 'react'
+import { motion, useAnimation, Variants, AnimationControls } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
+
+export const animationVariants = {
+  Visible: 'visible',
+  Hidden: 'hidden'
+}
+
+interface ScrollAnimatedDivProps {
+  children: React.ReactNode;
+  variants: Variants;
+  initial: string;
+  inViewCallback: (control: AnimationControls) => void;
+  containerClassName: string;
+}
+
+export const ScrollAnimatedDiv = ({
+  variants,
+  initial = animationVariants.Hidden,
+  inViewCallback,
+  children,
+  containerClassName
+}: ScrollAnimatedDivProps) => {
+  const animationControl = useAnimation()
+
+  const [ref, inView] = useInView()
+
+  useEffect(() => {
+    if (inView) {
+      inViewCallback(animationControl)
+    }
+  }, [animationControl, inView])
+
+  return (
+    <motion.div
+      ref={ref}
+      variants={variants}
+      initial={initial}
+      animate={animationControl}
+      className={containerClassName}>
+      {children}
+    </motion.div>
+  )
+}
